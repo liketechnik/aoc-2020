59ca8c2550bf004e6208d93dafedea7311f69eec
Florian Warzecha <liketechnik@disroot.org>
2020-12-01 09:40:37 +0100

    FIX(2020,1): skip unneccessary elements

--- 2020/day1_2/src/main.rs
+++ 2020/day1_2/src/main.rs
@@ -17,29 +17,32 @@ fn main() -> Result<()> {
     let sums = expenses
         .iter()
         .enumerate()
-        // create tuples of ((expense index, expense), (expenses index, expenses))
-        // mapping each expense to each expense
+        // create unique tuples of expense to expense to expense mapping
         .flat_map(|input1| {
-            expenses
-                .iter()
-                .enumerate()
+            ((input1.0 + 1)..) // enumerate would start at 0, but we need the 'real' index below
+                .zip(
+                    expenses
+                        // skip the first 'n' elements, because they were already included before
+                        // (if the index of input1's iteration is 5, we already have a all possible
+                        // mappings for index 1, because addition doesn't care about the order of operators)
+                        .split_at(input1.0)
+                        .1
+                        .iter(),
+                )
                 .map(move |input2| (input1, input2))
         })
         .flat_map(|input12| {
             expenses
+                .split_at(std::cmp::max(input12.0 .0, input12.1 .0))
+                .1
                 .iter()
-                .enumerate()
-                .map(move |input3| (input12.0, input12.1, input3))
+                .map(move |input3| (input12.0 .1, input12.1 .1, input3))
         })
         // filter out mappings of exactly the same value (determined by index)
         // and keep only the ones which sum to 2020
         .filter_map(|input| {
-            if input.0 .0 != input.1 .0
-                && input.0 .0 != input.2 .0
-                && input.1 .0 != input.2 .0
-                && input.0 .1 + input.1 .1 + input.2 .1 == 2020
-            {
-                Some((input.0 .1, input.1 .1, input.2 .1))
+            if input.0 + input.1 + input.2 == 2020 {
+                Some((input.0, input.1, input.2))
             } else {
                 None
             }
